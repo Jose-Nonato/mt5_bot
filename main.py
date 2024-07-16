6f10ca932bf653373bea108bf74f80c08c0ccd9a
José Nonato <josnonato161@gmail.com>
2024-07-16 18:45:50 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,33 @@
+import json
+import os
+import mt5_lib
+
+
+def get_project_settings(filepath):
+    if os.path.exists(filepath):
+        f = open(filepath, 'r')
+        data_user = json.load(f)
+        f.close()
+        return data_user
+    else:
+        raise ImportError("filepath doesn't exist")
+
+
+def start_up(project_settings):
+    """
+    Function to start up procedures for App. Includes starting/testing initializing symbols and anything else to ensure
+    app start is succesfully launched.
+    :param project_settings: json object of the project settings
+    :return: Boolean. True if app start is successfully launched, otherwise False
+    """
+    startup = mt5_lib.start_mt5(project_settings)
+    if startup:
+        print("MetaTrader startup succesfully!")
+        return True
+    return False
+
+
+if __name__ == '__main__':
+    project_settings = get_project_settings("project_settings.json")
+    start = start_up(project_settings)
+    print(start)
---
+++ mt5_lib.py
@@ -0,0 +1,47 @@
+import MetaTrader5
+
+
+def start_mt5(project_settings):
+    """
+    Function to start MetaTrader5
+    :return: Boolean: True = Started, False = not Started
+    """
+    username = int(project_settings['mt5']['username'])
+    password = project_settings['mt5']['password']
+    server = project_settings['mt5']['server']
+    mt5_pathway = project_settings['mt5']['mt5_pathway']
+
+    mt5_init = False
+    try:
+        mt5_init = MetaTrader5.initialize(
+            login=username,
+            password=password,
+            server=server
+        )
+    except Exception as e:
+        print(f'Error initializing Metatrader5: {e}')
+        mt5_init = False
+
+    mt5_login = False
+    if mt5_init:
+        try:
+            mt5_login = MetaTrader5.login(
+                login=username,
+                password=password,
+                server=server
+            )
+        except Exception as e:
+            print(f'Error logging into Metatrader5: {e}')
+            mt5_login = False
+
+    if mt5_login:
+        return True
+    return False
+
+
+def initialize_symbol(symbol):
+    """
+    Function to initialize symbol on MT5. Assumes that MT5 is already initialized.
+    :param symbol: string of symbol.
+    :return: Boolean: True = Initialized, False = not Initialized
+    """
